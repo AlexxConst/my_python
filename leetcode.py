9618f74333cc152daaf01e6365fcca711566bcd3
Aleksandr Suslin <asuslin01@gmail.com>
2024-11-02 08:47:26 +0200

    "initial commit"

---
+++ leetcode.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def makeFancyString(self, s):
+        s = s.lower()
+        stack = []
+
+        for char in s:
+            if stack and stack[-1][0] == char:
+                count = min(stack[-1][1] + 1, 2)
+                stack[-1] = (char, count)
+            else:
+                stack.append((char, 1))
+
+        result = "".join(char * count for char, count in stack)
+        return result
+
+
+solution = Solution()
+
+res = solution.makeFancyString("Vvvaaaaalllllleeeerrrraaaaa")
+print(res)
